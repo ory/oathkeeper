283a2e0b5e62df8f3ff091d455d7f65c16e34090
arekkas <aeneas@ory.am>
2017-11-09 22:11:06 +0100

    Add HTTPS capabilities and document proxy/management commands

---
+++ cmd/helper_messages.go
@@ -0,0 +1,39 @@
+package cmd
+
+var corsMessage = `CORS CONTROLS
+==============
+- CORS_ALLOWED_ORIGINS: A list of origins (comma separated values) a cross-domain request can be executed from.
+	If the special * value is present in the list, all origins will be allowed. An origin may contain a wildcard (*)
+	to replace 0 or more characters (i.e.: http://*.domain.com). Usage of wildcards implies a small performance penality.
+	Only one wildcard can be used per origin. The default value is *.
+	Example: CORS_ALLOWED_ORIGINS=http://*.domain.com,http://*.domain2.com
+
+- CORS_ALLOWED_METHODS: A list of methods  (comma separated values) the client is allowed to use with cross-domain
+	requests. Default value is simple methods (GET and POST).
+	Example: CORS_ALLOWED_METHODS=POST,GET,PUT
+
+- CORS_ALLOWED_CREDENTIALS: Indicates whether the request can include user credentials like cookies, HTTP authentication
+	or client side SSL certificates. The default is false.
+
+- CORS_DEBUG: Debugging flag adds additional output to debug server side CORS issues.
+
+- CORS_MAX_AGE: Indicates how long (in seconds) the results of a preflight request can be cached. The default is 0 which stands for no max age.
+
+- CORS_ALLOWED_HEADERS: A list of non simple headers (comma separated values) the client is allowed to use with cross-domain requests.
+
+- CORS_EXPOSED_HEADERS: Indicates which headers (comma separated values) are safe to expose to the API of a CORS API specification.`
+
+var databaseUrl = `- DATABASE_URL: A URL to a persistent backend. Hydra supports various backends:
+  - Memory: If DATABASE_URL is "memory", data will be written to memory and is lost when you restart this instance.
+  	Example: DATABASE_URL=memory
+
+  - Postgres: If DATABASE_URL is a DSN starting with postgres:// PostgreSQL will be used as storage backend.
+	Example: DATABASE_URL=postgres://user:password@host:123/database
+
+	If PostgreSQL is not serving TLS, append ?sslmode=disable to the url:
+	DATABASE_URL=postgres://user:password@host:123/database?sslmode=disable
+
+  - MySQL: If DATABASE_URL is a DSN starting with mysql:// MySQL will be used as storage backend.
+	Example: DATABASE_URL=mysql://user:password@tcp(host:123)/database?parseTime=true
+
+	Be aware that the ?parseTime=true parameter is mandatory, or timestamps will not work.`
