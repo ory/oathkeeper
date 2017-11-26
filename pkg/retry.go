7a58ce50a88170eceba559efaada88226581ec7e
arekkas <aeneas@ory.am>
2017-11-26 16:05:40 +0100

    Upgrades vendor dependencies

---
+++ pkg/retry.go
@@ -0,0 +1,48 @@
+// Copyright © 2017 Aeneas Rekkas <aeneas+oss@aeneas.io>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pkg
+
+import (
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+)
+
+func Retry(logger logrus.FieldLogger, maxWait time.Duration, failAfter time.Duration, f func() error) (err error) {
+	var lastStart time.Time
+	err = errors.New("Did not connect.")
+	loopWait := time.Millisecond * 100
+	retryStart := time.Now()
+	for retryStart.Add(failAfter).After(time.Now()) {
+		lastStart = time.Now()
+		if err = f(); err == nil {
+			return nil
+		}
+
+		if lastStart.Add(maxWait * 2).Before(time.Now()) {
+			retryStart = time.Now()
+		}
+
+		LogError(err, logger)
+		logger.Infof("Retrying in %f seconds...", loopWait.Seconds())
+		time.Sleep(loopWait)
+		loopWait = loopWait * time.Duration(int64(2))
+		if loopWait > maxWait {
+			loopWait = maxWait
+		}
+	}
+	return err
+}
