e1f6ab86f89a11b207f18c2381aef326792757c2
arekkas <aeneas@ory.am>
2018-02-28 18:49:52 +0100

    scripts: Use source file from vendor for mock generation

--- scripts/run-mock.sh
+++ scripts/run-mock.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
-mockgen -package evaluator -destination evaluator/hydra_sdk_mock.go github.com/ory/hydra/sdk/go/hydra SDK
+mockgen -package evaluator -destination evaluator/hydra_sdk_mock.go -source ./vendor/github.com/ory/hydra/sdk/go/hydra/sdk_api.go SDK
