e123d1321f86a1ebbb390ae59e74bff25e1b89a1
aeneasr <aeneas@ory.sh>
2019-04-01 12:02:37 +0200

    u
    Signed-off-by: aeneasr <aeneas@ory.sh>

--- cmd/tools.go
+++ cmd/tools.go
@@ -3,11 +3,12 @@
 package cmd
 
 import (
+	_ "github.com/go-swagger/go-swagger/cmd/swagger"
 	_ "github.com/mattn/goveralls"
 	_ "github.com/mitchellh/gox"
-	_ "github.com/ory/go-acc"
 	_ "github.com/tcnksm/ghr"
 	_ "golang.org/x/tools/cmd/cover"
-	_ "github.com/go-swagger/go-swagger/cmd/swagger"
 	_ "golang.org/x/tools/cmd/goimports"
+
+	_ "github.com/ory/go-acc"
 )
