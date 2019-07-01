67face611b9f19ed9b6606931c9b7a82df769154
Igor Zibarev <hypnoglow@gmail.com>
2019-07-01 13:11:52 +0300

    Add support for rules in YAML format (#213)
    This commit adds support for defining access rules in YAML format, in
    addition to existing JSON format.

--- rule/fetcher_default_test.go
+++ rule/fetcher_default_test.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/ory/oathkeeper/driver/configuration"
 	"github.com/ory/oathkeeper/internal"
+	"github.com/ory/oathkeeper/rule"
 )
 
 const testRule = `[{"id":"test-rule-5","upstream":{"preserve_host":true,"strip_path":"/api","url":"mybackend.com/api"},"match":{"url":"myproxy.com/api","methods":["GET","POST"]},"authenticators":[{"handler":"noop"},{"handler":"anonymous"}],"authorizer":{"handler":"allow"},"mutator":{"handler":"noop"}}]`
@@ -64,6 +65,61 @@ func TestFetcher(t *testing.T) {
 				tc.expectIDs = []string{}
 			}
 			assert.EqualValues(t, ids, tc.expectIDs)
+		})
+	}
+}
+
+func TestFetcherDefaultFetchFormats(t *testing.T) {
+	expected := []rule.Rule{
+		{
+			ID: "test-rule-1",
+			Match: rule.RuleMatch{
+				Methods: []string{"GET", "POST"},
+				URL:     "myproxy.com/api",
+			},
+			Authenticators: []rule.RuleHandler{
+				{
+					Handler: "noop",
+				},
+				{
+					Handler: "anonymous",
+				},
+			},
+			Authorizer: rule.RuleHandler{
+				Handler: "allow",
+			},
+			Mutator: rule.RuleHandler{
+				Handler: "noop",
+			},
+			Upstream: rule.Upstream{
+				PreserveHost: true,
+				StripPath:    "/api",
+				URL:          "mybackend.com/api",
+			},
+		},
+	}
+
+	testCases := map[string]struct {
+		fpath string
+	}{
+		"json file": {
+			fpath: "testdata/rules.json",
+		},
+		"yaml file": {
+			fpath: "testdata/rules.yaml",
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			conf := internal.NewConfigurationWithDefaults()
+			viper.Set(configuration.ViperKeyAccessRuleRepositories, []string{"file://" + tc.fpath})
+
+			r := internal.NewRegistry(conf)
+			rules, err := r.RuleFetcher().Fetch()
+			require.NoError(t, err)
+
+			assert.Equal(t, expected, rules)
 		})
 	}
 }
