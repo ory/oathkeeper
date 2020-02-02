f84aa7d98206092976181048c258f39557a4c68e
aeneasr <aeneas@ory.sh>
2020-02-02 22:34:04 +0000

    chore: Regenerate swagger spec and internal client

--- internal/httpclient/client/api/list_rules_responses.go
+++ internal/httpclient/client/api/list_rules_responses.go
@@ -14,7 +14,7 @@ import (
 
 	strfmt "github.com/go-openapi/strfmt"
 
-	models "github.com/ory/oathkeeper/internal/httpclient/models"
+	"github.com/ory/oathkeeper/internal/httpclient/models"
 )
 
 // ListRulesReader is a Reader for the ListRules structure.
