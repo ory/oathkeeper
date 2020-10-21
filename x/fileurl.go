1360892a01ebfea81ba4bb9edb2d011abd155c65
Patrik Lindahl <patrik@makersign.com>
2020-10-21 17:59:54 +0800

    New ParseURL function and fixes to GetURLFilePath The PatseURL function will handle parsing URLs with special handling of file:// URLs The updated GetURLFilePath function will return a correct path baed on the updated URL parsing.

--- x/fileurl.go
+++ x/fileurl.go
@@ -3,32 +3,95 @@ package x
 import (
 	"net/url"
 	"path/filepath"
+	"regexp"
 	"runtime"
+	"strings"
+
+	"github.com/ory/x/logrusx"
 )
 
+// winPathRegex is a regex for [DRIVE-LETTER]:
+var winPathRegex = regexp.MustCompile("^[A-Za-z]:.*")
+
+// winUNCRegex is a regex for UNC path (starts with \\)
+var winUNCRegex = regexp.MustCompile("^\\\\\\\\.*")
+
+// winUNCRegex is a regex for file://
+var fileRegex = regexp.MustCompile("^(?i)file://")
+
 // GetURLFilePath returns the path of a URL that is compatible with the runtime os filesystem
-func GetURLFilePath(u url.URL) string {
+func GetURLFilePath(u *url.URL) string {
+	if u == nil {
+		return ""
+	}
+
+	if u.Scheme != "file" && u.Scheme != "" {
+		return u.Path
+	}
 	fPath := u.Path
-	sep := string(filepath.Separator)
-	// Special case for malformed file urls (file://../relative/path, file://path)
-	// this means we cannot support the hostname part of the URL as an actual hostname
-	fPath = u.Host + fPath
-	if runtime.GOOS == "windows" {
-		// Strip any path initial separator on windows
+	if u.Host != "" {
+		// On POSIX systems this will return a UNC style path like //host/share/path
+		// this will probably not be very useful
+		s := string(filepath.Separator)
+		fPath = s + s + u.Host + filepath.Clean(filepath.FromSlash(fPath))
+		return fPath
+	}
+	if runtime.GOOS == "windows" && winPathRegex.MatchString(fPath[1:]) {
+		// On Windows we should remove the initial path separator in case this
+		// is a normal path (for example: "\c:\" -> "c:\"")
 		fPath = stripFistPathSeparators(fPath)
 	}
-	fPath = filepath.FromSlash(fPath)
+	return filepath.Clean(filepath.FromSlash(fPath))
+}
 
-	// Special case for non standard relative paths
-	isRelative := (len(fPath) > 1 && fPath[0:1] == ".") ||
-		(len(fPath) > 2 && fPath[0:2] == sep+".") ||
-		(len(fPath) > 3 && fPath[0:3] == sep+sep+".")
-	if isRelative {
-		// Strip first path separator so we can return a relative path
-		return stripFistPathSeparators(fPath)
+// ParseURL parses rawurl into a URL structure with special handling for file:// URLs
+func ParseURL(rawurl string) (*url.URL, error) {
+	lcRawurl := strings.ToLower(rawurl)
+	if strings.Index(lcRawurl, "file:///") == 0 {
+		return url.Parse(rawurl)
 	}
+	if fileRegex.MatchString(rawurl) {
+		// Normally the first part after file:// is a hostname, but since
+		// this is often misused we interpret the URL like a normal path
+		// by removing the "file://" from the beginning
+		rawurl = rawurl[7:]
+	}
+	if winPathRegex.MatchString(rawurl) {
+		// Windows path
+		return url.Parse("file:///" + toSlash(rawurl))
+	}
+	if winUNCRegex.MatchString(rawurl) {
+		// Windows UNC path
+		// We extract the hostname and creates an appropriate file:// URL
+		// based on the hostname and the path
+		parts := strings.Split(filepath.FromSlash(rawurl), "\\")
+		host := ""
+		if len(parts) > 2 {
+			host = parts[2]
+		}
+		p := "/"
+		if len(parts) > 4 {
+			p += strings.Join(parts[3:], "/")
+		}
+		return url.Parse("file://" + host + p)
+	}
+	u, err := url.Parse(rawurl)
+	if err != nil {
+		return nil, err
+	}
+	if u.Scheme == "file" || u.Scheme == "" {
+		u.Path = toSlash(u.Path)
+	}
+	return u, nil
+}
 
-	return filepath.Clean(fPath)
+// ParseOrFatal parses a url or fatals.
+func ParseOrFatal(l *logrusx.Logger, in string) *url.URL {
+	out, err := ParseURL(in)
+	if err != nil {
+		l.WithError(err).Fatalf("Unable to parse url: %s", in)
+	}
+	return out
 }
 
 func stripFistPathSeparators(fPath string) string {
@@ -36,4 +99,8 @@ func stripFistPathSeparators(fPath string) string {
 		fPath = fPath[1:]
 	}
 	return fPath
+}
+
+func toSlash(path string) string {
+	return strings.ReplaceAll(path, "\\", "/")
 }
--- x/fileurl_test.go
+++ x/fileurl_test.go
@@ -1,49 +1,94 @@
 package x
 
 import (
-	"net/url"
 	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-type testData struct {
-	urlStr              string
-	expectedPathUnix    string
-	expectedPathWindows string
-	shouldSucceed       bool
-}
-
-var testURLs = []testData{
-	{"file:///home/test/file1.txt", "/home/test/file1.txt", "home\\test\\file1.txt", true},                                                    // Unix style
-	{"file:/home/test/file2.txt", "/home/test/file2.txt", "home\\test\\file2.txt", true},                                                      // File variant without slashes
-	{"file:///../test/update/file3.txt", "../test/update/file3.txt", "..\\test\\update\\file3.txt", true},                                     // Special case relative path
-	{"file://../test/update/file4.txt", "../test/update/file4.txt", "..\\test\\update\\file4.txt", true},                                      // Invalid relative path
-	{"file://C:/users/test/file5.txt", "C:/users/test/file5.txt", "C:\\users\\test\\file5.txt", true},                                         // Non standard Windows style
-	{"file:///C:/users/test/file5.txt", "/C:/users/test/file5.txt", "C:\\users\\test\\file5.txt", true},                                       // Windows style
-	{"file://anotherhost/share/users/test/file6.txt", "/share/users/test/file6.txt", "\\\\anotherhost\\share\\users\\test\\file6.txt", false}, // Windows style with hostname, this is not supported
-	{"file://file7.txt", "file7.txt", "file7.txt", true},                                                                                      // Invalid relative path
-	{"file://path/file8.txt", "path/file8.txt", "path\\file8.txt", true},                                                                      // Invalid relative path
-}
-
-func TestFileURL(t *testing.T) {
+func TestGetURLFilePath(t *testing.T) {
+	type testData struct {
+		urlStr          string
+		expectedUnix    string
+		expectedWindows string
+		shouldSucceed   bool
+	}
+	var testURLs = []testData{
+		{"File:///home/test/file1.txt", "/home/test/file1.txt", "\\home\\test\\file1.txt", true},
+		{"fIle:/home/test/file2.txt", "/home/test/file2.txt", "\\home\\test\\file2.txt", true},
+		{"fiLe:///../test/update/file3.txt", "/test/update/file3.txt", "\\test\\update\\file3.txt", true},
+		{"filE://../test/update/file4.txt", "../test/update/file4.txt", "..\\test\\update\\file4.txt", true},
+		{"file://C:/users/test/file5.txt", "/C:/users/test/file5.txt", "C:\\users\\test\\file5.txt", true},
+		{"file:///C:/users/test/file5b.txt", "/C:/users/test/file5b.txt", "C:\\users\\test\\file5b.txt", true},
+		{"file://anotherhost/share/users/test/file6.txt", "/share/users/test/file6.txt", "\\\\anotherhost\\share\\users\\test\\file6.txt", false}, // this is not supported
+		{"file://file7.txt", "file7.txt", "file7.txt", true},
+		{"file://path/file8.txt", "path/file8.txt", "path\\file8.txt", true},
+		{"file://C:\\Users\\RUNNER~1\\AppData\\Local\\Temp\\9ccf9f68-121c-451a-8a73-2aa360925b5a386398343/access-rules.json", "/C:/Users/RUNNER~1/AppData/Local/Temp/9ccf9f68-121c-451a-8a73-2aa360925b5a386398343/access-rules.json", "C:\\Users\\RUNNER~1\\AppData\\Local\\Temp\\9ccf9f68-121c-451a-8a73-2aa360925b5a386398343\\access-rules.json", true},
+		{"file8.txt", "file8.txt", "file8.txt", true},
+		{"../file9.txt", "../file9.txt", "..\\file9.txt", true},
+		{"..\\file10.txt", "../file10.txt", "..\\file10.txt", true},
+		{"C:\\file11.txt", "/C:/file11.txt", "C:\\file11.txt", true},
+		{"\\\\hostname\\share\\file12.txt", "//hostname/share/file12.txt", "\\\\hostname\\share\\file12.txt", true},
+		{"file:///home/test/file 13.txt", "/home/test/file 13.txt", "\\home\\test\\file 13.txt", true},
+		{"file:///home/test/file%2014.txt", "/home/test/file 14.txt", "\\home\\test\\file 14.txt", true},
+		{"http://server:80/test/file%2015.txt", "/test/file 15.txt", "/test/file 15.txt", true},
+	}
 	for _, td := range testURLs {
-		u, err := url.Parse(td.urlStr)
+		u, err := ParseURL(td.urlStr)
 		assert.NoError(t, err)
-		p := GetURLFilePath(*u)
+		p := GetURLFilePath(u)
 		if runtime.GOOS == "windows" {
 			if td.shouldSucceed {
-				assert.Equal(t, td.expectedPathWindows, p)
+				assert.Equal(t, td.expectedWindows, p)
 			} else {
-				assert.NotEqual(t, td.expectedPathWindows, p)
+				assert.NotEqual(t, td.expectedWindows, p)
 			}
 		} else {
 			if td.shouldSucceed {
-				assert.Equal(t, td.expectedPathUnix, p)
+				assert.Equal(t, td.expectedUnix, p)
 			} else {
-				assert.NotEqual(t, td.expectedPathUnix, p)
+				assert.NotEqual(t, td.expectedUnix, p)
 			}
 		}
+	}
+	assert.Empty(t, GetURLFilePath(nil))
+}
+
+func TestParseURL(t *testing.T) {
+	type testData struct {
+		urlStr       string
+		expectedPath string
+		expectedStr  string
+	}
+	var testURLs = []testData{
+		{"File:///home/test/file1.txt", "/home/test/file1.txt", "file:///home/test/file1.txt"},
+		{"fIle:/home/test/file2.txt", "/home/test/file2.txt", "file:///home/test/file2.txt"},
+		{"fiLe:///../test/update/file3.txt", "/../test/update/file3.txt", "file:///../test/update/file3.txt"},
+		{"filE://../test/update/file4.txt", "../test/update/file4.txt", "../test/update/file4.txt"},
+		{"file://C:/users/test/file5.txt", "/C:/users/test/file5.txt", "file:///C:/users/test/file5.txt"},  // We expect a initial / in the path because this is a Windows absolute path
+		{"file:///C:/users/test/file6.txt", "/C:/users/test/file6.txt", "file:///C:/users/test/file6.txt"}, // --//--
+		{"file://file7.txt", "file7.txt", "file7.txt"},
+		{"file://path/file8.txt", "path/file8.txt", "path/file8.txt"},
+		{"file://C:\\Users\\RUNNER~1\\AppData\\Local\\Temp\\9ccf9f68-121c-451a-8a73-2aa360925b5a386398343/access-rules.json", "/C:/Users/RUNNER~1/AppData/Local/Temp/9ccf9f68-121c-451a-8a73-2aa360925b5a386398343/access-rules.json", "file:///C:/Users/RUNNER~1/AppData/Local/Temp/9ccf9f68-121c-451a-8a73-2aa360925b5a386398343/access-rules.json"},
+		{"file://C:\\Users\\path with space\\file.txt", "/C:/Users/path with space/file.txt", "file:///C:/Users/path%20with%20space/file.txt"},
+		{"file8b.txt", "file8b.txt", "file8b.txt"},
+		{"../file9.txt", "../file9.txt", "../file9.txt"},
+		{"..\\file10.txt", "../file10.txt", "../file10.txt"},
+		{"C:\\file11.txt", "/C:/file11.txt", "file:///C:/file11.txt"},
+		{"\\\\hostname\\share\\file12.txt", "/share/file12.txt", "file://hostname/share/file12.txt"},
+		{"\\\\", "/", "file:///"},
+		{"\\\\hostname", "/", "file://hostname/"},
+		{"\\\\hostname\\", "/", "file://hostname/"},
+		{"file:///home/test/file 13.txt", "/home/test/file 13.txt", "file:///home/test/file%2013.txt"},
+		{"file:///home/test/file%2014.txt", "/home/test/file 14.txt", "file:///home/test/file%2014.txt"},
+		{"http://server:80/test/file%2015.txt", "/test/file 15.txt", "http://server:80/test/file%2015.txt"},
+	}
+
+	for _, td := range testURLs {
+		u, err := ParseURL(td.urlStr)
+		assert.NoError(t, err)
+		assert.Equal(t, td.expectedPath, u.Path)
+		assert.Equal(t, td.expectedStr, u.String())
 	}
 }
