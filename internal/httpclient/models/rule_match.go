0c537b48dd35985d679f533dba2501d891053909
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-07-19 10:18:42 +0000

    autogen(openapi): Regenerate swagger spec and internal client

--- internal/httpclient/models/rule_match.go
+++ internal/httpclient/models/rule_match.go
@@ -6,6 +6,8 @@ package models
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
+	"context"
+
 	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
 )
@@ -34,6 +36,11 @@ type RuleMatch struct {
 
 // Validate validates this rule match
 func (m *RuleMatch) Validate(formats strfmt.Registry) error {
+	return nil
+}
+
+// ContextValidate validates this rule match based on context it is used
+func (m *RuleMatch) ContextValidate(ctx context.Context, formats strfmt.Registry) error {
 	return nil
 }
 
--- internal/httpclient/models/upstream.go
+++ internal/httpclient/models/upstream.go
@@ -6,6 +6,8 @@ package models
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
+	"context"
+
 	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
 )
@@ -28,6 +30,11 @@ type Upstream struct {
 
 // Validate validates this upstream
 func (m *Upstream) Validate(formats strfmt.Registry) error {
+	return nil
+}
+
+// ContextValidate validates this upstream based on context it is used
+func (m *Upstream) ContextValidate(ctx context.Context, formats strfmt.Registry) error {
 	return nil
 }
 
