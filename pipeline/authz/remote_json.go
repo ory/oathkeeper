a5574893429892fa39c9481222b559ee02495762
Neakxs <37744447+Neakxs@users.noreply.github.com>
2021-12-02 22:08:22 +0100

    Add unix support for remote authorizer

--- pipeline/authz/remote_json.go
+++ pipeline/authz/remote_json.go
@@ -43,7 +43,7 @@ type AuthorizerRemoteJSON struct {
 func NewAuthorizerRemoteJSON(c configuration.Provider) *AuthorizerRemoteJSON {
 	return &AuthorizerRemoteJSON{
 		c:      c,
-		client: httpx.NewResilientClientLatencyToleranceSmall(nil),
+		client: httpx.NewResilientClientLatencyToleranceSmall(helper.NewRoundTripper()),
 		t:      x.NewTemplate("remote_json"),
 	}
 }
