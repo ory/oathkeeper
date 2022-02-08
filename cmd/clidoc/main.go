962f57eeea8310e5b970da420819e0e268d8da4d
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-02-08 15:50:24 +0100

    ci: update cli location and fix generation script

--- cmd/clidoc/main.go
+++ cmd/clidoc/main.go
@@ -4,13 +4,11 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/ory/x/clidoc"
-
 	"github.com/ory/oathkeeper/cmd"
 )
 
 func main() {
-	if err := clidoc.Generate(cmd.RootCmd, os.Args[1:]); err != nil {
+	if err := Generate(cmd.RootCmd, os.Args[1:]); err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "%+v", err)
 		os.Exit(1)
 	}
