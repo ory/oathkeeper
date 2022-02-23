f275d5c948a4139aa6063b014df466b048f7538f
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-02-23 14:26:25 +0100

    autogen: pin v0.38.21-beta.1.pre.0 release commit

---
+++ docs/.docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/Users/foobar/workspace/go/src/github.com/ory/oathkeeper/docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/foobar/workspace/go/src/github.com/ory/oathkeeper/docs/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/foobar/workspace/go/src/github.com/ory/oathkeeper/docs/src/plugins/docusaurus-plugin-matamo/analytics"),
+  require("/Users/foobar/workspace/go/src/github.com/ory/oathkeeper/docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/foobar/workspace/go/src/github.com/ory/oathkeeper/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/foobar/workspace/go/src/github.com/ory/oathkeeper/docs/src/css/theme.css"),
+];
---
+++ docs/.docusaurus/routes.js
@@ -0,0 +1,250 @@
+
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+export default [
+{
+  path: '/oathkeeper/docs/search',
+  component: ComponentCreator('/oathkeeper/docs/search','5f4'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/versions',
+  component: ComponentCreator('/oathkeeper/docs/versions','da0'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/next',
+  component: ComponentCreator('/oathkeeper/docs/next','fb8'),
+  
+  routes: [
+{
+  path: '/oathkeeper/docs/next/',
+  component: ComponentCreator('/oathkeeper/docs/next/','2a3'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/next/api-access-rules',
+  component: ComponentCreator('/oathkeeper/docs/next/api-access-rules','1b1'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/next/configure-deploy',
+  component: ComponentCreator('/oathkeeper/docs/next/configure-deploy','9ff'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/next/contributing',
+  component: ComponentCreator('/oathkeeper/docs/next/contributing','735'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/next/install',
+  component: ComponentCreator('/oathkeeper/docs/next/install','8a8'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/next/milestones',
+  component: ComponentCreator('/oathkeeper/docs/next/milestones','a76'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/next/oauth2-token-introspection-hydra',
+  component: ComponentCreator('/oathkeeper/docs/next/oauth2-token-introspection-hydra','442'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/next/pipeline',
+  component: ComponentCreator('/oathkeeper/docs/next/pipeline','517'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/next/pipeline/authn',
+  component: ComponentCreator('/oathkeeper/docs/next/pipeline/authn','775'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/next/pipeline/authz',
+  component: ComponentCreator('/oathkeeper/docs/next/pipeline/authz','732'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/next/pipeline/error',
+  component: ComponentCreator('/oathkeeper/docs/next/pipeline/error','4d4'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/next/pipeline/mutator',
+  component: ComponentCreator('/oathkeeper/docs/next/pipeline/mutator','41a'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/next/reference/api',
+  component: ComponentCreator('/oathkeeper/docs/next/reference/api','a03'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/next/reference/configuration',
+  component: ComponentCreator('/oathkeeper/docs/next/reference/configuration','56a'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/next/sdk',
+  component: ComponentCreator('/oathkeeper/docs/next/sdk','3f7'),
+  exact: true,
+},
+]
+},
+{
+  path: '/oathkeeper/docs/v0.37',
+  component: ComponentCreator('/oathkeeper/docs/v0.37','db2'),
+  
+  routes: [
+{
+  path: '/oathkeeper/docs/v0.37/',
+  component: ComponentCreator('/oathkeeper/docs/v0.37/','e46'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/v0.37/api-access-rules',
+  component: ComponentCreator('/oathkeeper/docs/v0.37/api-access-rules','ec6'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/v0.37/configure-deploy',
+  component: ComponentCreator('/oathkeeper/docs/v0.37/configure-deploy','2ec'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/v0.37/install',
+  component: ComponentCreator('/oathkeeper/docs/v0.37/install','52a'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/v0.37/pipeline/authn',
+  component: ComponentCreator('/oathkeeper/docs/v0.37/pipeline/authn','c8c'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/v0.37/pipeline/authz',
+  component: ComponentCreator('/oathkeeper/docs/v0.37/pipeline/authz','31e'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/v0.37/pipeline/error',
+  component: ComponentCreator('/oathkeeper/docs/v0.37/pipeline/error','ede'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/v0.37/pipeline/index',
+  component: ComponentCreator('/oathkeeper/docs/v0.37/pipeline/index','c96'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/v0.37/pipeline/mutator',
+  component: ComponentCreator('/oathkeeper/docs/v0.37/pipeline/mutator','b92'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/v0.37/reference/api',
+  component: ComponentCreator('/oathkeeper/docs/v0.37/reference/api','f8c'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/v0.37/reference/configuration',
+  component: ComponentCreator('/oathkeeper/docs/v0.37/reference/configuration','c6c'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/v0.37/sdk/index',
+  component: ComponentCreator('/oathkeeper/docs/v0.37/sdk/index','0eb'),
+  exact: true,
+},
+]
+},
+{
+  path: '/oathkeeper/docs/',
+  component: ComponentCreator('/oathkeeper/docs/','d34'),
+  
+  routes: [
+{
+  path: '/oathkeeper/docs/',
+  component: ComponentCreator('/oathkeeper/docs/','b95'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/api-access-rules',
+  component: ComponentCreator('/oathkeeper/docs/api-access-rules','538'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/configure-deploy',
+  component: ComponentCreator('/oathkeeper/docs/configure-deploy','ae6'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/contributing',
+  component: ComponentCreator('/oathkeeper/docs/contributing','0f0'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/install',
+  component: ComponentCreator('/oathkeeper/docs/install','c30'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/milestones',
+  component: ComponentCreator('/oathkeeper/docs/milestones','393'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/oauth2-token-introspection-hydra',
+  component: ComponentCreator('/oathkeeper/docs/oauth2-token-introspection-hydra','130'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/pipeline',
+  component: ComponentCreator('/oathkeeper/docs/pipeline','980'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/pipeline/authn',
+  component: ComponentCreator('/oathkeeper/docs/pipeline/authn','d19'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/pipeline/authz',
+  component: ComponentCreator('/oathkeeper/docs/pipeline/authz','acd'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/pipeline/error',
+  component: ComponentCreator('/oathkeeper/docs/pipeline/error','c24'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/pipeline/mutator',
+  component: ComponentCreator('/oathkeeper/docs/pipeline/mutator','d18'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/reference/api',
+  component: ComponentCreator('/oathkeeper/docs/reference/api','51a'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/reference/configuration',
+  component: ComponentCreator('/oathkeeper/docs/reference/configuration','4ea'),
+  exact: true,
+},
+{
+  path: '/oathkeeper/docs/sdk',
+  component: ComponentCreator('/oathkeeper/docs/sdk','782'),
+  exact: true,
+},
+]
+},
+{
+  path: '*',
+  component: ComponentCreator('*')
+}
+];
