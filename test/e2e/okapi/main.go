2b13ac12ebf9ee482d11a50790158381ebc8f91e
Henning Perl <henning.perl@gmail.com>
2023-01-17 14:52:21 +0100

    chore(deps): bump JWT deps (#1052)

--- test/e2e/okapi/main.go
+++ test/e2e/okapi/main.go
@@ -4,43 +4,41 @@
 package main
 
 import (
-	"crypto/rsa"
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 
-	jwtmiddleware "github.com/auth0/go-jwt-middleware"
-	"github.com/form3tech-oss/jwt-go"
+	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
+	"github.com/auth0/go-jwt-middleware/v2/jwks"
+	"github.com/auth0/go-jwt-middleware/v2/validator"
 	"github.com/julienschmidt/httprouter"
 	"github.com/urfave/negroni"
-	"gopkg.in/square/go-jose.v2"
 
 	"github.com/ory/oathkeeper/x"
 	"github.com/ory/x/urlx"
 )
 
-var jwtm = jwtmiddleware.New(jwtmiddleware.Options{
-	ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-		u := x.ParseURLOrPanic(os.Getenv("OATHKEEPER_API"))
-		res, err := http.Get(urlx.AppendPaths(u, "/.well-known/jwks.json").String())
-		if err != nil {
-			panic(err)
-		}
-		defer res.Body.Close()
-		if res.StatusCode != 200 {
-			panic("not 200")
-		}
+var (
+	jwksProvider *jwks.Provider
+	jwtValidator *validator.Validator
+)
 
-		var jwks jose.JSONWebKeySet
-		if err := json.NewDecoder(res.Body).Decode(&jwks); err != nil {
-			panic(err)
-		}
+func init() {
+	var err error
+	u := x.ParseURLOrPanic(os.Getenv("OATHKEEPER_API"))
+	jwksProvider = jwks.NewProvider(urlx.AppendPaths(u, "/.well-known/jwks.json"))
+	jwtValidator, err = validator.New(
+		jwksProvider.KeyFunc,
+		validator.RS256,
+		jwksProvider.IssuerURL.String(),
+		[]string{},
+	)
+	if err != nil {
+		panic(err)
+	}
+}
 
-		return jwks.Key(token.Header["kid"].(string))[0].Key.(*rsa.PublicKey), nil
-	},
-	SigningMethod: jwt.SigningMethodRS256,
-})
+var jwtm = jwtmiddleware.New(jwtValidator.ValidateToken)
 
 func main() {
 	router := httprouter.New()
@@ -54,6 +52,9 @@ func main() {
 
 	n := negroni.Classic()
 	n.UseHandler(router)
+	n.UseFunc(func(_ http.ResponseWriter, _ *http.Request, next http.HandlerFunc) {
+		jwtm.CheckJWT(next)
+	})
 	server := http.Server{
 		Addr:    fmt.Sprintf(":%s", port),
 		Handler: n,
@@ -64,9 +65,6 @@ func main() {
 	}
 }
 
-func jwtHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	if err := jwtm.CheckJWT(w, r); err != nil {
-		return
-	}
+func jwtHandler(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 	_, _ = w.Write([]byte("ok"))
 }
