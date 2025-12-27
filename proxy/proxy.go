f7c1e850c4a4187c6f30982571b795c1e16e518a
joao.folgado <jfolgado94@gmail.com>
2025-12-27 15:17:13 +0000

    format

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -130,7 +130,7 @@ func (d *Proxy) Rewrite(r *httputil.ProxyRequest) {
 	// maintain transparency and pass through the original query parameters to allow backends
 	// to handle them according to their own validation rules.
 	r.Out.URL.RawQuery = r.In.URL.RawQuery
-	
+
 	rl, err := d.r.RuleMatcher().Match(r.Out.Context(), r.Out.Method, r.Out.URL, rule.ProtocolHTTP)
 	if err != nil {
 		*r.Out = *r.Out.WithContext(context.WithValue(r.Out.Context(), director, err))
